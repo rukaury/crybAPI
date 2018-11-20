0794fd386223fd6159cd7b66631ce3d07b53ec0d
Aury Rukazana <production.outspace@gmail.com>
2018-11-20 15:17:45 -0500

    Change of DB authentication

--- crybAPI/library/DB.php
+++ crybAPI/library/DB.php
@@ -10,7 +10,7 @@ class DB
    static function get() {
        if(self::$instance == null) {
            try {
-               self::$instance = new PDO('mysql:host=localhost;dbname=cryb', 'crybUser', 'gng4120');
+               self::$instance = new PDO('mysql:host=localhost;dbname=cryb', 'quadtech', 'seg3102');
                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
            } catch(PDOException $e) {
                throw new Exception("Unable to connect to the database - " . $e->getMessage(), 500);
