de19c54c78b580dd9561197a716d6ca98a76f446
Aury Rukazana <production.outspace@gmail.com>
2018-11-20 20:22:51 -0500

    Initial API implementation of the OPR system

---
+++ controllers/PropertiesController.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rukau
+ * Date: 2018-11-10
+ * Time: 22:31
+ */
+
+class PropertiesController
+{
+    private $model;
+    private $model_name;
+
+    public function __construct() {
+        preg_match("/(.+)Controller$/", get_class($this), $match);
+        $this->model_name = $match[1] . "Model";
+        if (class_exists($this->model_name)) {
+            $this->model = new $this->model_name();
+        } else {
+            throw new Exception("Model does not exist.", 500);
+        }
+    }
+
+    /*
+     @uri	/Properties
+     @verb	GET
+     @desc	Get a list of Properties
+     */
+    /*
+     @uri	/Properties/{id}
+     @verb	GET
+     @desc	Get one property
+     */
+    public function getAction($request) {
+        if (!empty($request->url_elements[3])) {
+            $property_query = $request->url_elements[3];
+            if ($property_query != 'options' && $property_query != 'owner'){
+                // crybAPI/properties/{id}
+                return $this->model->getProperty((int)$property_query);
+            }
+            elseif ($property_query == 'options'){
+                // crybAPI/properties/options/{city}/{type}/{#of_bedrooms}/{#of_bathrooms}/{minPrice}/{maxPrice}
+                $property_options = array();
+                if(isset($request->url_elements[4]) && $request->url_elements[4] != 'none'){
+                    $property_options['city'] = $request->url_elements[4] ;
+                }
+                if(isset($request->url_elements[5]) && $request->url_elements[5] != 'none'){
+                    $property_options['type'] = $request->url_elements[5];
+                }
+                if(isset($request->url_elements[6]) && (int)$request->url_elements[6] != 0){
+                    $property_options['num_of_bedrooms'] = $request->url_elements[6];
+                }
+                if(isset($request->url_elements[7]) && (int)$request->url_elements[7] != 0){
+                    $property_options['num_of_bathrooms'] = $request->url_elements[7];
+                }
+                if(isset($request->url_elements[8]) && (int)$request->url_elements[8] != 0){
+                    $property_options['minPrice'] = $request->url_elements[8];
+                }
+                if(isset($request->url_elements[9]) && (int)$request->url_elements[9] != 0){
+                    $property_options['maxPrice'] = $request->url_elements[9];
+                }
+                return $this->model->getPropertiesByOptions($property_options);
+            }
+            elseif ($property_query == 'owner'){
+                // crybAPI/properties/owner/{id}
+                //}/{maxPrice}
+                return $this->model->getPropertiesByOwner((int)$request->url_elements[4]);
+            }
+            else
+                throw new Exception("Invalid property info.", 400);
+        } else {
+            // crybAPI/Properties
+            return $this->model->getProperties();
+        }
+    }
+
+    /*
+     @uri	/Properties
+     @verb	POST
+     @desc	Create one property
+     */
+    public function postAction($request) {
+        // crybAPI/Properties
+        $this->model = Helper::cast($request->body->property, $this->model_name);
+        if ($this->model->first_name && $this->model->last_name && $this->model->email)
+            return $this->model->createProperty();
+        else
+            throw new Exception("Invalid or missing property object in request.", 400);
+    }
+
+    /*
+     @uri	/Properties
+     @verb	PUT
+     @desc	Update one property
+     */
+    public function putAction($request) {
+        // crybAPI/Properties
+        $this->model = Helper::cast($request->body->property, $this->model_name);
+        if ($this->model->id)
+            return $this->model->updateProperty();
+        else
+            throw new Exception("Invalid or missing property object in request.", 400);
+    }
+
+    /*
+     @uri	/Properties/{id}
+     @verb	DELETE
+     @desc	Delete one property
+     */
+    public function deleteAction($request) {
+        // crybAPI/Properties/{id}
+        if (($request->url_elements[3])) {
+            $property_id = (int)$request->url_elements[3];
+            if ($property_id)
+                return $this->model->deleteProperty($property_id);
+            else
+                throw new Exception("Invalid property ID.", 400);
+        } else {
+            throw new Exception("Missing property ID.", 400);
+        }
+    }
+}
---
+++ controllers/PropertyPhotosController.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rukau
+ * Date: 2018-11-10
+ * Time: 22:41
+ */
+
+class PropertyPhotosController
+{
+    private $model;
+    private $model_name;
+
+    public function __construct() {
+        preg_match("/(.+)Controller$/", get_class($this), $match);
+        $this->model_name = $match[1] . "Model";
+        if (class_exists($this->model_name)) {
+            $this->model = new $this->model_name();
+        } else {
+            throw new Exception("Model does not exist.", 500);
+        }
+    }
+
+    /*
+     @uri	/PropertyPhotos
+     @verb	GET
+     @desc	Get a list of property photos
+     */
+    /*
+     @uri	/PropertyPhotos/{id}
+     @verb	GET
+     @desc	Get all property photos from property
+     */
+    public function getAction($request) {
+        if (!empty($request->url_elements[3])) {
+            $property_id = (int)$request->url_elements[3];
+            if (is_int($property_id) && $request->url_elements[4] == 'allData')
+                // API/PropertyPhotos/{id}
+                return $this->model->getPropertyPhotos($property_id);
+            elseif (is_int($property_id) && $request->url_elements[4] == 'number')
+                // API/PropertyPhotos/{id}
+                return $this->model->getPropertyPhotosNumber($property_id);
+            else
+                throw new Exception("Invalid property ID.", 400);
+        } else {
+            // API/propertyPhotos
+            return $this->model->getAllPropertyPhotos();
+        }
+    }
+
+    /*
+     @uri	/PropertyPhotos
+     @verb	POST
+     @desc	Create one PropertyPhoto
+     */
+    public function postAction($request) {
+        // API/propertyPhotos
+        $this->model = Helper::cast($request->body->property, $this->model_name);
+        if ($this->model->first_name && $this->model->last_name && $this->model->email)
+            return $this->model->createPropertyPhoto();
+        else
+            throw new Exception("Invalid or missing property object in request.", 400);
+    }
+
+    /*
+     @uri	/PropertyPhotos
+     @verb	PUT
+     @desc	Update one property
+     */
+    public function putAction($request) {
+        // API/propertyPhotos
+        $this->model = Helper::cast($request->body->property, $this->model_name);
+        if ($this->model->id)
+            return $this->model->updatePropertyPhoto();
+        else
+            throw new Exception("Invalid or missing property object in request.", 400);
+    }
+
+    /*
+     @uri	/PropertyPhotos/{id}
+     @verb	DELETE
+     @desc	Delete one property
+     */
+    public function deleteAction($request) {
+        // API/propertyPhotos/{id}
+        if (($request->url_elements[3])) {
+            $property_id = (int)$request->url_elements[3];
+            if ($property_id)
+                return $this->model->deletePropertyPhoto($property_id);
+            else
+                throw new Exception("Invalid property ID.", 400);
+        } else {
+            throw new Exception("Missing property ID.", 400);
+        }
+    }
+}
